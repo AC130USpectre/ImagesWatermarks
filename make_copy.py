1fd63018309431c04d172d91b40f6af40bec45aa
Ruslan Yusupov, MIPT-211 <hunter9999999@yandex.ru>
2015-11-17 23:15:34 +0300

    Create make_copy.py

---
+++ make_copy.py
@@ -0,0 +1,3 @@
+from ImposeNoise import imposeNoise
+
+imposeNoise(input("Путь к оригиналу: "), input("Путь к копии: "), int(input("Номер пользователя: ")) - 1, int(input("Число пользователей: ")), int(input("Ширина кластера: ")), int(input("Высота кластера: ")))
