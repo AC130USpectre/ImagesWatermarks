90e4b7ed3814acfa21f19af3bbbaa252c3214675
Ruslan Yusupov, MIPT-211 <hunter9999999@yandex.ru>
2015-11-17 23:14:36 +0300

    Create getMaxSignedInt.c

---
+++ getMaxSignedInt.c
@@ -0,0 +1,8 @@
+#include <limits.h>
+
+/*возвращаем платформо-зависимый INT_MAX. Используется для нахождения максимально возможного значения seed*/
+
+unsigned int main(int argc, char** argv)
+{
+    return INT_MAX;
+}
